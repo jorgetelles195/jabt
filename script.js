9cfbbf9acc09193228e5a28c24950cdacacaf7d4
jorgetelles195 <dr@jorgetelles.com>
2025-04-21 22:40:04 -0300

    Update script.js
    script do Excel

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function calcular() {
-  var dbp = document.getElementById('dbp').value;
+  var dbp = document.getElementById('dbp').value; // capturamos o valor, mas não usamos
   var cc = document.getElementById('cc').value;
   var ca = document.getElementById('ca').value;
   var cf = document.getElementById('cf').value;
@@ -12,19 +12,9 @@ function calcular() {
   cc = Number(cc);
   ca = Number(ca);
   cf = Number(cf);
-  dbp = Number(dbp);
 
-  var peso = 0;
-
-  if (dbp > 0) {
-    // Fórmula Hadlock 4 parâmetros
-    var log10peso = 1.3596 + (0.0064 * cc) + (0.0424 * ca) + (0.174 * cf) + (0.00061 * cc * cf) - (0.00386 * ca * cf);
-    peso = Math.pow(10, log10peso);
-  } else {
-    // Fórmula Hadlock 3 parâmetros (sem DBP)
-    var log10peso = 1.335 - (0.0034 * cc) + (0.0316 * ca) + (0.0457 * cf);
-    peso = Math.pow(10, log10peso);
-  }
+  var log10peso = 1.326 - (0.00326 * cf * ca) + (0.0107 * cc) + (0.0438 * cf) + (0.158 * ca);
+  var peso = Math.pow(10, log10peso);
 
   if (isNaN(peso) || peso < 0 || peso > 10000) {
     document.getElementById('resultado').innerText = "Dados inválidos. Verifique os valores.";
@@ -40,3 +30,4 @@ function limpar() {
   document.getElementById('cf').value = "";
   document.getElementById('resultado').innerText = "";
 }
+
